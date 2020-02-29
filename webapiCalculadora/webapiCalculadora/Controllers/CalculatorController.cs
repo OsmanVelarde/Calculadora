b8acbf08a7a94f8eda605fd479c7081b4cb098c1
osmastersyco@gmail.com <osmastersyco@gmail.com>
2020-02-29 10:25:25 -0400

    suma y multiplicacion

---
+++ webapiCalculadora/webapiCalculadora/Controllers/CalculatorController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webapiCalculadora.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        public CalculatorController()
+        {
+
+        }
+
+        [Route("add")]
+        [HttpPost]
+        public int Add([FromHeader]int a, [FromHeader]int b, int c)
+        {
+            return a + b;
+        }
+        [Route("add")]
+        [HttpGet]
+        public int Add(int a,int b)
+        {
+            return a + b;
+        }
+        [Route("multiply")]
+        [HttpPost]
+        public int Multiply([FromHeader]int a, [FromHeader]int b, int c)
+        {
+            return a * b;
+        }
+        [Route("multiply")]
+        [HttpGet]
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
